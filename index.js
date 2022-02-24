29809be8388b0e36acfac34a70fb3ec193d140ba
HRMxan <hrmxan@gmail.com>
2022-02-24 15:03:17 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
+    polling: true
+});
+
+
+const sendChat = -767862069;
+bot.onText(/\/start/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(sendChat, `${JSON.stringify(msg)}`);
+    console.log(msg);
+});
+bot.on('message', msg => {
+    const chatId = msg.chat.id;
+    if (chatId === sendChat) {
+        const sentUser = JSON.parse(msg.reply_to_message.text).id;
+        bot.sendMessage(sentUser, msg.text);
+    } else {
+        bot.sendMessage(sendChat, `${JSON.stringify(
+            {
+                id: msg.chat.id,
+                name: msg.chat.first_name + ' ' + msg.chat.last_name,
+            }
+        )}`);
+        bot.sendMessage(sendChat, msg.text);
+    }
+});
